091c8a53ba80c30aca1b3a7797ff75b941f6e0d7
HoXuanHungg <xuanhungho1998@gmail.com>
2022-04-13 11:24:06 +0700

    Làm thêm:  -Tìm kiếm theo tên có hiện thông báo.  -Thêm user check sdt.

--- forum/src/main/java/com/dg3/forum/forum/repository/UserstRepository.java
+++ forum/src/main/java/com/dg3/forum/forum/repository/UserstRepository.java
@@ -1,11 +1,13 @@
 package com.dg3.forum.forum.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -13,11 +15,14 @@ import com.dg3.forum.forum.entity.Users;
 
 @Repository
 
-
 public interface UserstRepository extends JpaRepository<Users, Long> {
 	@Query("SELECT u from Users u where u.user_pk = :user_pk")
 	Users findRoomByUserId(@PathVariable("user_pk") Long user_pk);
 
-//	@Query("DELETE  from Users u where u.user_pk = :user_pk")
-//	Optional<Users> deleteAccount(@PathVariable("user_pk") Long user_pk);
+	//findByUsername*
+	List<Users> findByUsername(String username);
+	//existByPhone_number*
+	@Query("SELECT u from Users u where u.phone_number = :phone_number")
+	List<Users> existByPhone_number(String phone_number);
+
 } 
