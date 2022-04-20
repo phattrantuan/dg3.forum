f8b3ef5763bf54246c30ae1ec2d49c821bd6b4df
Nguyen160900 <nguyen4224hn@gmail.com>
2022-04-20 16:53:19 +0700

    New

---
+++ forum/src/main/java/com/dg3/forum/forum/entity/Users.java
@@ -0,0 +1,80 @@
+package com.dg3.forum.forum.entity;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.dg3.forum.forum.customannotation.Phone;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(value = { "roles", "authorities" })
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_pk;
+    private String email;
+    private String password;
+    private String username;
+    private String role;
+    @NotBlank
+    @Phone
+    private String phone_number;
+    private String address;
+    private Date date_of_birth;
+    private boolean ban_account;
+    private String img_avatar;
+    private String description;
+    private Date created_date;
+    private Date expire;
+    private boolean enable_users;
+    public Users(String email, String password, String username, String role, String phone_number, String address,
+                 Date date_of_birth, boolean ban_account, String img_avatar, String description, Date created_date,
+                 Date expire, boolean enable_users) {
+        super();
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.role = role;
+        this.phone_number = phone_number;
+        this.address = address;
+        this.date_of_birth = date_of_birth;
+        this.ban_account = ban_account;
+        this.img_avatar = img_avatar;
+        this.description = description;
+        this.created_date = created_date;
+        this.expire = expire;
+        this.enable_users = enable_users;
+    }
+    public List<GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+        authorities.add(new SimpleGrantedAuthority(role));
+
+        return authorities;
+    }
+
+
+}
