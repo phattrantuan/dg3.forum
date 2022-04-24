63b830fd4b5654e5c5addcfc8ced9321030ef824
quangtruongtran <85612038+truongtep197@users.noreply.github.com>
2022-04-24 23:19:05 +0700

    login, signup user API

--- forum/src/main/java/com/dg3/forum/forum/config/WebSecurityConfig.java
+++ forum/src/main/java/com/dg3/forum/forum/config/WebSecurityConfig.java
@@ -63,7 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		httpSecurity.csrf().disable()
 				// don't authenticate this particular request
 				.authorizeRequests()
-					.antMatchers("/authenticate", "/signin")
+					.antMatchers("/authenticate", "/signin", "/api/v1/users/login","/api/v1/users/sign-up")
 					.permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
